e01b396f990555be5efe9368199ce02bfc1df6ca
Sergio Almecija <sergio.almecija@tapjoy.com>
2012-11-06 14:34:53 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PixelsDoc::Application.config.secret_token = '347c51dec19aa0bf3b69fae2afd157aaff20c2316c63bbc73812cec6cfddf02fdf70edab2392e1450ea445fab4de8e58a7c7a49f7583fbfab27cc5fc391c445d'
